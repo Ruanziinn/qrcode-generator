2d4f4b3b4842d6f30cf283ad9a4625e9f703b0df
Ruanziin <ruanpeixotorip@gmail.com>
2025-04-26 16:05:21 -0300

    Criação do /service e conclusão das responsabilidades e funcionalidades do projeto

--- src/main/java/com/ruanziin/qrcode_generator/controller/QrCodeController.java
+++ src/main/java/com/ruanziin/qrcode_generator/controller/QrCodeController.java
@@ -2,6 +2,7 @@ package com.ruanziin.qrcode_generator.controller;
 
 import com.ruanziin.qrcode_generator.dto.qrcode.QrCodeGenerateRequest;
 import com.ruanziin.qrcode_generator.dto.qrcode.QrCodeGenerateResponse;
+import com.ruanziin.qrcode_generator.service.QrCodeGenerateService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,11 +11,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/qrcode")
-
 public class QrCodeController {
+
+    private final QrCodeGenerateService qrCodeGenerateService;
+
+    public QrCodeController(QrCodeGenerateService qrCodeService) {
+        this.qrCodeGenerateService = qrCodeService;
+    }
 
     @PostMapping
     public ResponseEntity<QrCodeGenerateResponse> generate(@RequestBody QrCodeGenerateRequest request){
-        return null;
+        try {
+            QrCodeGenerateResponse response = this.qrCodeGenerateService.generateAndUploadQrCode(request.text());
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
     }
 }
